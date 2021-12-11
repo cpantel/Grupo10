cf27c71f221583a232632c14dbda7686540639b3
Carlos Pantelides <carlos.pantelides@bbva.com>
2021-12-11 17:01:19 -0300

    A8: CSRF fixed on profile

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-// const csrf = require('csurf');
+const csrf = require('csurf');
 const consolidate = require("consolidate"); // Templating library adapter for Express
 const swig = require("swig");
 const helmet = require("helmet");
@@ -107,6 +107,15 @@ MongoClient.connect(db, (err, db) => {
         */
 
     }));
+
+    app.use(csrf());
+
+    app.use( function(req,res,next) {
+           res.locals.csrftoken = req.csrfToken();
+           next();
+        }
+    );
+
 
     /*
     // Fix for A8 - CSRF
